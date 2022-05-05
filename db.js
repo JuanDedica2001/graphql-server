4fbd23c5f01d39190490808001bf153b8f604170
JuanDedica2001 <juanjose.hernandez@dedica.com.mx>
2022-05-05 15:17:59 -0500

    first versión of the back

--- db.js
+++ db.js
@@ -1,48 +1,65 @@
+const { v1 } = require('uuid');
+
 //create an array of book objects
 const books = [
   {
+    id: v1(),
     title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling'
+    author: 'J.K. Rowling',
+    numberOfPages: 341
   }, 
   {
+    id: v1(),
     title: 'Jurassic Park',
-    author: 'Michael Crichton'
+    author: 'Michael Crichton', 
+    numberOfPages: 456
   },
   {
+    id: v1(),
     title: 'The Awakening',
     author: 'Kate Chopin',
   },
   {
+    id: v1(),
     title: 'City of Glass',
     author: 'Paul Auster',
   },
   {
+    id: v1(),
     title: 'The Great Gatsby',
     author: 'F. Scott Fitzgerald',
   },
   {
+    id: v1(),
     title: 'The Hobbit',
     author: 'J.R.R. Tolkien',
   },
   {
+    id: v1(),
     title: 'The Hunger Games',
     author: 'Suzanne Collins',
+    numberOfPages: 374
   }, 
   {
+    id: v1(),
     title: 'The Catcher in the Rye',
     author: 'J.D. Salinger',
   }, 
   {
+    id: v1(),
     title: 'The Fault in Our Stars',
     author: 'John Green',
   }, 
   {
+    id: v1(),
     title: 'The Kite Runner',
     author: 'Khaled Hosseini',
   },
   {
+    id: v1(),
     title: 'The Alchemist',
     author: 'Paulo Coelho',
+    numberOfPages: 163
   },
 ]
 
--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-const {ApolloServer, gql} = require('apollo-server');
-const {books} = require('./db.js');
-
-
-
+const { ApolloServer, gql } = require('apollo-server');
+const { books } = require('./db.js');
+const { v1 } = require('uuid');
 
 const typeDefs = gql`
   type Book {
+    id: ID
     title: String!
     author: String
+    numberOfPages: Int
   }
   type Query {
     allBooks: [Book]
     numberOfBooks: Int
+    findBooks(title: String!): [Book]
+  }
+  type Mutation {
+    addBook(title: String!, author: String, numberOfPages: Int): Book
   }
 `
 
 const resolvers = {
   Query: {
     allBooks: () => books,
-    numberOfBooks: () => books.length
+    numberOfBooks: () => books.length,
+    findBooks: (root, args) => {
+      return books.filter(book => book.title.toLowerCase().includes(args.title.toLowerCase()));
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const {title, author, numberOfPages} = args;
+      const book = {
+        id: v1(),
+        title,
+        author,
+        numberOfPages
+      }
+      books.push(book);
+      return book;
+    }
   }
 }
 
